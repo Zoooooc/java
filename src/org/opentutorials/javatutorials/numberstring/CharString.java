1747b97b4dfa68d28f5bbc32a234bb7775ac7a6a
juseungwon <97448626+juseungwon@users.noreply.github.com>
2023-04-05 13:25:30 +0900

    char/string

---
+++ src/org/opentutorials/javatutorials/numberstring/CharString.java
@@ -0,0 +1,16 @@
+package org.opentutorials.javatutorials.numberstring;
+
+public class CharString {
+
+	public static void main(String[] args) {
+		System.out.println('꼬');
+		
+		// 꼬부기는 문자열이므로 '' > ""
+//		System.out.println('꼬부기');
+		
+		System.out.println("꼬부기");
+		
+		System.out.println("꼬");
+	}
+
+}
