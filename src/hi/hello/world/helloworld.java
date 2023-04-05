7a0cf99ede75ec1d68b5b3a7fdd3d4388fade10a
juseungwon <97448626+juseungwon@users.noreply.github.com>
2023-04-05 11:14:07 +0900

    test

--- src/hi/hello/world/helloworld.java
+++ src/hi/hello/world/helloworld.java
@@ -3,13 +3,7 @@ package hi.hello.world;
 public class helloworld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("hello world");
-		System.out.println("hello world");
-		System.out.println("hello world");
-		System.out.println("hello world");
-		// 무야~~~~~~~~~~~ 왜 되는데
-		// 짜증나네~~!~!~!~!~!
+		System.out.println("수정갈기자");
 	}
 
 }
