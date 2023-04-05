77561deb4d8c2dbea65aadbbedad1748c60cd9d4
juseungwon <97448626+juseungwon@users.noreply.github.com>
2023-04-05 13:37:35 +0900

    study

--- src/org/opentutorials/javatutorials/numberstring/CharString.java
+++ src/org/opentutorials/javatutorials/numberstring/CharString.java
@@ -11,6 +11,16 @@ public class CharString {
 //		System.out.println('꼬부기');
 		System.out.println("꼬");
 		
+		// 더하기 연산자로 문자열 연결 가능
+		System.out.println("꼬부" + "기");
+		
+		// 이스케이프
+		// \를  " 앞에 붙여 " 의 문법적인 역할을 이스케이프해서 문자로 인식하게함
+		System.out.println("꼬부기 : \"꼬북꼬북\""); // 꼬부기 : "꼬북꼬북"
+
+		
+		
+		
 	}
 
 }
