8e5a0e71aa7a9e40bf76a963c56b6e4cc800b4ad
juseungwon <97448626+juseungwon@users.noreply.github.com>
2023-04-05 13:29:00 +0900

    study

--- src/org/opentutorials/javatutorials/numberstring/CharString.java
+++ src/org/opentutorials/javatutorials/numberstring/CharString.java
@@ -3,14 +3,14 @@ package org.opentutorials.javatutorials.numberstring;
 public class CharString {
 
 	public static void main(String[] args) {
+		
 		System.out.println('꼬');
+		System.out.println("꼬부기");
 		
 		// 꼬부기는 문자열이므로 '' > ""
 //		System.out.println('꼬부기');
-		
-		System.out.println("꼬부기");
-		
 		System.out.println("꼬");
+		
 	}
 
 }
