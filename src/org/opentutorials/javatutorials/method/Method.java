89c643a903ea76ef0444cc605cf406d4e9fe1761
juseungwon <97448626+juseungwon@users.noreply.github.com>
2023-04-06 23:26:01 +0900

    study

---
+++ src/org/opentutorials/javatutorials/method/Method.java
@@ -0,0 +1,16 @@
+package org.opentutorials.javatutorials.method;
+
+public class Method {
+
+	public static void main(String[] args) {
+		numbering();
+	}
+	
+	public static void numbering() {
+		int i = 0;
+		while(i < 10) {
+			System.out.println(i);
+			i++;
+		}
+	}
+}
